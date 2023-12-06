eb27d8d3425ffb330d5aa7cd4a5f0c621844e25e
cozenom <david.p646@gmail.com>
2023-12-05 17:30:39 -0800

    p2 WIP

---
+++ day5.py
@@ -0,0 +1,97 @@
+data = open("day5.txt").read().strip()
+data = open("test.txt").read()
+data = data.split('\n\n')
+
+for i, row in enumerate(data):
+    if row[:5] == 'seeds':
+        row = row.split(': ')
+        row[1] = [int(i) for i in row[1].split(' ')]
+    else:
+        row = row.split(':\n')
+        # row[0] = row[0].split('-to-')
+        row[1] = row[1].split('\n')
+
+        for j, rec in enumerate(row[1]):
+            row[1][j] = [int(i) for i in rec.split(' ')]
+    data[i] = row
+
+# Part 1
+# What is the lowest location number that corresponds to any of the initial seed numbers?
+start = data[0][1]
+for map in data[1:]:
+    pre = dict([(i, i) for i in start])
+    post = []
+    ranges = map[1]
+    # the destination range start, the source range start, and the range length.
+    for r in ranges:
+        dest, source, rlength = r[0], r[1], r[2]
+        sourcerange = range(source, source + rlength)
+        for i in pre.keys():
+            if i in sourcerange:
+                pre[i] = i - source + dest
+    start = list(pre.values())
+
+print('Part 1: ', min(start))
+
+
+# Part 2
+# the seeds: line actually describes ranges of seed numbers
+
+def getOverlap(input, sourcerange):
+    # Range
+    x1, x2, y1, y2 = input[0], input[-1]+1, sourcerange[0], sourcerange[-1]+1
+    r = [y1<x1, y1<x2, y2<x1, y2<x2]
+    res = []
+    if sum(r)==1:
+        # R
+        pos = input.index(y1)
+        res = [range(x1, y1), range(y1,x2),1]
+    elif sum(r)==2:
+        # Both
+        if r[1] and r[3]:
+            pos, pos1 = input.index(y1), input.index(y2)
+            res = [range(x1,y1), range(y1,y2), range(y2,x2),1]
+        if r[0] and r[1]:
+            res = [input, 0]
+    elif sum(r)==3:
+        # L
+        pos = input.index(y2)
+        res = [range(x1,y2), range(y2,x2),0]
+    else:
+        # Neither
+        res = [input]
+    return res
+
+
+start = []
+
+for i in range(len(data[0][1]) // 2):
+    a, b = data[0][1][2 * i], data[0][1][2 * i + 1]
+    start.append([a, b])
+
+ranges = []
+for pair in start:
+    ranges.append(range(pair[0], pair[0] + pair[1]))
+
+start = ranges
+for mapping in data[1:]:
+    pre = dict([(i, i) for i in start])
+    ranges = mapping[1]
+    post = []
+    # the destination range start, the source range start, and the range length.
+    print(mapping[0], ranges)
+    for r in ranges:
+        dest, source, rlength = r[0], r[1], r[2]
+        sourcerange = range(source, source + rlength)
+        for i in pre.keys():
+            o = getOverlap(i, sourcerange)
+            if len(o)>1:
+                # Overlapped
+                print(sourcerange, i, o)
+                p = o[-1]
+                res = o[:-1]
+                res[p] = range(dest - source + res[p][0], dest + (res[p][1] - res[p][0]))
+                print(res)
+
+
+    start = list(pre.values())
