a03639dc767448ad3573c12dd4b41943168af69c
David <david.p646@gmail.com>
2023-12-08 21:50:46 -0800

    day9 sexy recursion

---
+++ day9.py
@@ -0,0 +1,32 @@
+data = open("day9.txt").read().strip()
+data = data.split('\n')
+data = [[int(j) for j in i.split()] for i in data]
+
+# Part 1
+def getNext(l: list):
+    diffs = [l[i+1]-l[i] for i,j in enumerate(l[:-1])]
+    # base case
+    if sum(diffs)==0:
+        return l[0]
+    return getNext(diffs)+l[-1]
+
+res = 0
+for seq in data:
+    res += getNext(seq)
+
+print('Part 1: ', res)
+
+# Part 2
+def getPrev(l:list):
+    diffs = [l[i+1]-l[i] for i,j in enumerate(l[:-1])]
+    # base case
+    if sum(diffs) == 0:
+        return l[0]
+
+    return l[0]-getPrev(diffs)
+
+res = 0
+for seq in data:
+    res += getPrev(seq)
+
+print('Part 2: ', res)
